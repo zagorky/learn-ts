ad8d585b613b762335bc063a3f27cf6a3c77e9f4
zagorky <zagorkiynahyi@gmail.com>
2025-02-19 15:46:03 +0300

    feat: add 31 lesson

---
+++ learn-ts/lessons/more about types/31 structural typing.ts
@@ -0,0 +1,26 @@
+type DataState = {
+  status: LoadingStatus;
+  error?: Error;
+  data?: number;
+}
+
+enum LoadingStatus {
+  Loading = 'loading...',
+  Error = 'error',
+  Success = 'success',
+
+}
+
+function handleData(state: DataState): string {
+  switch (state.status) {
+    case LoadingStatus.Loading:
+      return LoadingStatus.Loading;
+    case LoadingStatus.Error:
+      return LoadingStatus.Error;
+    case LoadingStatus.Success:
+      return state.data?.toString() || 'unknown';
+    default:
+      return 'unknown';
+  }
+}
+
