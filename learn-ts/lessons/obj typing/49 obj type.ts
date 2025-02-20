5c886d0559be702a8041746dc7831bb097542a6e
zagorky <zagorkiynahyi@gmail.com>
2025-02-20 18:15:55 +0300

    feat: add 49 lesson

---
+++ learn-ts/lessons/obj typing/49 obj type.ts
@@ -0,0 +1,13 @@
+function extract(object: object, keys: string[]): object {
+  const entries = Object.entries(object).filter(([key]) => keys.includes(key));
+
+  return Object.fromEntries(entries);
+}
+
+const user = {
+  name: 'Tirion',
+  email: 'tirion@lanister.got',
+  age: 35,
+}
+
+console.log(extract(user, ['name', 'age'])); // { name: 'Tirion', age: 35 }
